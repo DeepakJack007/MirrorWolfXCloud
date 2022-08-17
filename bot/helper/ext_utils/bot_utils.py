8be12f93c1dc4962eab22df385f7005a279f52f6
Deepak Jack <93731368+DeepakJack007@users.noreply.github.com>
2022-08-17 16:19:41 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -21,15 +21,16 @@ PAGE_NO = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Up📤"
-    STATUS_DOWNLOADING = "Down📥"
-    STATUS_CLONING = "Cloning♻️"
-    STATUS_WAITING = "Queued...💤"
-    STATUS_PAUSE = "Paused...⛔️"
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
-    STATUS_SPLITTING = "Splitting...✂️"
-    STATUS_CHECKING = "CheckingUp...📝"
+        STATUS_UPLOADING = "Uploading...📤"
+        STATUS_DOWNLOADING = "Downloading...📥"
+        STATUS_CLONING = "Cloning...♻️"
+        STATUS_WAITING = "Queued...💤"
+        STATUS_PAUSED = "Paused...⛔️"
+        STATUS_ARCHIVING = "Archiving...🔐"
+        STATUS_EXTRACTING = "Extracting...📂"
+        STATUS_SPLITTING = "Splitting...✂️"
+        STATUS_CHECKING = "CheckingUp...📝"
+        STATUS_SEEDING = "Seeding...🌧"
 
 class EngineStatus:
     STATUS_ARIA = "Aria2c"
